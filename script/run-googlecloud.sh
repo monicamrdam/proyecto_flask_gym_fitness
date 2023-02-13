5c48ac6a8f3111a88c65c0aabe0e9ba6ea625a25
Marta Barroso <mbarrosoisidoro@gmail.com>
2023-02-13 18:34:29 +0100

    Add gitignore

--- script/run-googlecloud.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-gcloud compute instances create myinstance --machine-type=n1-standard-1 --zone=us-central1-a  
-gcloud compute firewall-rules create allow-winrm --allow tcp:8080
