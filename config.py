de7bcee9faa607f2fdcb655df54181ecd7b4fb41
monicaMR <monicamrdam@gmail.com>
2023-02-13 17:29:11 +0100

    añadido config

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+# Carga las variables de entorno en memoria
+load_dotenv()  # Cargar todo el contenido de .env en variables de entorno
+
+
+class Config:
+    pass
